cd3882ea284a862312ada76669fdba173b6801aa
cabalex <31020729+cabalex@users.noreply.github.com>
2021-10-19 00:52:13 -0700

    Puzzle fixes, minor style adjustments
    --- PUZZLE
    - Completely fixed mobile support (only 8 terms on mobile, 16 on desktop)
    - Fixed bug where terms weren't randomized
    - Added fail animation
    ---
    - Made shuffle algorithm less biased
    - Fixed alignment issue and general UI tweaks on homepage

--- core/index.js
+++ core/index.js
@@ -1,12 +1,15 @@
 /* Global functions. */
+// Fisher-Yates Shuffle - https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 Array.prototype.shuffle = function() {
-    var i = this.length, j, temp;
-    if ( i == 0 ) return this;
-    while ( --i ) {
-       j = Math.floor( Math.random() * ( i + 1 ) );
-       temp = this[i];
-       this[i] = this[j];
-       this[j] = temp;
+    let currentIndex = this.length,  randomIndex;
+
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [this[currentIndex], this[randomIndex]] = [
+        this[randomIndex], this[currentIndex]];
     }
     return this;
 }
--- games/puzzle/puzzle.js
+++ games/puzzle/puzzle.js
@@ -1,11 +1,13 @@
 var startTime = 0;
-const shuffledCards = Object.keys(cards).shuffle().slice(0, 16);
-const shuffledAgain = shuffledCards.shuffle();
+const cardCount = Math.min(window.innerWidth > 700 ? 16 : 8, Object.keys(cards).length);
+const shuffledCards = Object.keys(cards).shuffle().slice(0, cardCount);
+// must deep copy, cannot pass by reference
+const shuffledAgain = JSON.parse(JSON.stringify(shuffledCards)).shuffle();
 var stopwatchInterval;
 
+// Starts the game.
 function startGame() {
     startTime = Date.now();
-    const cardCount = Math.min(16, shuffledCards.length);
     $('#gameModal').css('display', 'none');
     for (var i = 0; i < cardCount; i++) {
         $('#pg-1').append(`<div class="card q" id="q${i}"><p></p><img src="outer.svg"></div>`);
@@ -14,41 +16,50 @@ function startGame() {
     // shuffle the order of the 16 terms again
     for (var i = 0; i < cardCount; i++) {
         $('#pg-2').append(`<div class="card a" id="a${i}"><p></p><img src="inner.svg"></div>`);
-        $('#a' + i).find('p').text(cards[shuffledCards[i]])
+        $('#a' + i).find('p').text(cards[shuffledAgain[i]])
     }
-    $('.card:not(.a)').mousedown(dragger)
+    $('.card:not(.a)').on('mousedown', dragger).on('touchstart', dragger)
 
     // interval
     stopwatchInterval = setInterval(updateStopwatch, 100)
 }
 
+// Updates the #time stopwatch. 
 function updateStopwatch() {
     $('#time').text(milisecondsToTime(Date.now() - startTime));
 }
 
+// Ends the game and shows the game result screen.
 function endGame() {
     $('#finishModal').show();
     clearInterval(stopwatchInterval);
     $('#termCount').text(shuffledCards.length);
     $('#resultTime').text(milisecondsToTime(Date.now() - startTime))
+    if (cardCount < 16) {
+        $('#mobileAlert').show();
+    }
 }
 
+// Term dragging functionality. Supports both mobile (ontouchstart) and desktop (onmousedown)
 function dragger(event) {
     event.preventDefault()
     const elem = this;
     $(this).attr('class', 'card q dragging');
-    const start = [event.pageX, event.pageY];
+    // touch or click drag
+    const start = [event.pageX || event.touches[0].clientX, event.pageY || event.touches[0].clientY];
     var offset = $(this).offset();
+
     function handle_dragging(e){
-        var left = offset['left'] + (e.pageX - start[0]);
-        var top = offset['top'] + (e.pageY - start[1]);
+        var left = offset['left'] + ((e.pageX || e.touches[0].clientX) - start[0]);
+        var top = offset['top'] + ((e.pageY || e.touches[0].clientY) - start[1]);
         $(elem).offset({top: top, left: left});
     }
+
     function handle_mouseup(e){
-        $(document.body).off('mousemove', handle_dragging)
-        $(this).off('mouseup', handle_mouseup);
+        $(document.body).off('mousemove').off('touchmove')
+        $(this).off('mouseup').off('touchend');
         // check if over another card
-        const bounding = detectBounding(e.pageX, e.pageY);
+        const bounding = detectBounding(e.pageX || e.changedTouches[0].pageX, e.pageY || e.changedTouches[0].pageY);
         if (bounding != -1 && shuffledAgain[bounding] == shuffledCards[parseInt($(this).attr('id').slice(1))]) {
             $(this).attr('class', 'card-deleted q').off('mousedown');
             $('#a' + bounding).attr('class', 'card-deleted a');
@@ -58,14 +69,20 @@ function dragger(event) {
                 endGame();
             }
         } else {
+            if (bounding != -1 && !$('#a' + bounding).attr('class').includes('card-deleted')) {
+                $('#a' + bounding).css('animation', 'popIn 0.2s ease-out, shakeDeny 0.2s ease-in-out');
+                setTimeout(function() { $('#a' + bounding).css('animation', '') }, 200)
+            }
             $(this).attr('class', 'card q');
             $(this).css({'left': 0, 'top': 0})
         }
     }
-    $(this).on('mouseup', handle_mouseup)
-    $(document.body).on('mousemove', handle_dragging);
+
+    $(this).on('mouseup', handle_mouseup).on('touchend', handle_mouseup)
+    $(document.body).on('mousemove', handle_dragging).on("touchmove", handle_dragging);
 }
 
+// Detects if the cursor is inside a block. TODO: check bounding of term boxes instead of cursor for easier matching
 function detectBounding(cursorX, cursorY) {
     boundingRects = []
     $('.a').get().forEach((elem, index) => boundingRects[index] = checkCollisions(elem.getBoundingClientRect()))
